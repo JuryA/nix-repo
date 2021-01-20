4218015b4e9d6438ba503dec0efbb07bfd9d626e
Jiří Altman <altman.jiri@gmail.com>
2021-01-21 00:42:57 +0100

    first commit

--- default.nix
+++ default.nix
@@ -1,1 +1,12 @@
-(import ./nix { }).ci
+{ sources ? import ./sources.nix }: # import the sources
+with
+{
+  overlay = _:
+    {
+      niv = import sources.niv { }; # use the sources :)
+    };
+};
+import sources.nixpkgs                  # and use them again!
+{ overlays = [ overlay ]; config = { }; }
+
+  (import ./nix { }).ci
